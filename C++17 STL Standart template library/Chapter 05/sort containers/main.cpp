5940436d6371acdd9cb7a4b53d28f8a46e689e36
Konstantin Ignakov <ignakov37@gmail.com>
2021-05-27 16:57:58 +0300

    Use std::sort, std::is_sorted, std::shuffle, std::partial_sort and std::partition.

---
+++ C++17 STL Standart template library/Chapter 05/sort containers/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <random>
+
+using namespace std;
+
+static void print (vector<int> &v) {
+    copy(begin(v), end(v), ostream_iterator<int>{cout, ", "});
+    cout << '\n';
+}
+
+int main() {
+    vector v {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    random_device rd;
+    mt19937 g { rd() };
+
+    cout << "Is sorted: ";
+    cout << is_sorted(begin(v), end(v)) << '\n';
+
+    shuffle(begin(v), end(v), g);
+
+    cout << "Is sorted: ";
+    cout << is_sorted(begin(v), end(v)) << '\n';
+    print(v);
+
+    sort(begin(v), end(v));
+    cout << "Is sorted: ";
+    cout << is_sorted(begin(v), end(v)) << '\n';
+    print(v);
+
+    shuffle(begin(v), end(v), g);
+    partition(begin(v), end(v), [] (int i) { return i < 5; });
+    cout << "Partition sort: ";
+    print(v);
+
+    shuffle(begin(v), end(v), g);
+    auto middle (next(begin(v), int(v.size()) / 2));
+    partial_sort(begin(v), middle, end(v));
+    cout << "Partial sort: ";
+    print(v);
+
+
+    struct mystruct {
+        int a;
+        int b;
+    };
+    vector<mystruct> mv {
+            {5,     100},
+            {1,     50},
+            {-123,  1000},
+            {3,     70},
+            {-10,   20}
+    };
+
+    sort(begin(mv), end(mv), [] (const mystruct &lhs, const mystruct &rhs) {
+        return lhs.b < rhs.b;
+    });
+
+    cout << "My class sort (by second value): \n";
+    for (const auto &[a, b] : mv) {
+        cout << "{" << a << ", " << b << "} ";
+    }
+    cout << '\n';
+
+    return 0;
+}
