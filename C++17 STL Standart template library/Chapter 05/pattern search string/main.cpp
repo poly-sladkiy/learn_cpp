6f76f31ed9a14141aa7e4cc15a5651beb53cac63
Konstantin Ignakov <ignakov37@gmail.com>
2021-05-30 15:27:23 +0300

    Pattern search string: std::search, std::default_search, std::boyer_moore_searcher, std::boyer_moore_horspool_searcher

---
+++ C++17 STL Standart template library/Chapter 05/pattern search string/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+
+using namespace std;
+
+template<typename Itr>
+static void print(Itr it, size_t chars) {
+    copy_n(it, chars, ostream_iterator<char>{cout});
+    cout << '\n';
+}
+
+int main() {
+    const string long_string {
+        "Lorem ipsum dolor sit amet, consetetur"
+        " sadipscing elitr, sed diam nonumy eirmod"
+    };
+    const string needle {"elitr"};
+
+    {
+        auto match (search(begin(long_string), end(long_string),
+                           default_searcher(begin(needle), end(needle))));
+        print(match, 5);
+    }
+
+    {
+        auto match (search(begin(long_string), end(long_string),
+                           boyer_moore_searcher(begin(needle), end(needle))));
+        print(match, 5);
+    }
+
+    {
+        auto match (search(begin(long_string), end(long_string),
+                           boyer_moore_horspool_searcher(begin(needle), end(needle))));
+        print(match, 5);
+    }
+
+    return 0;
+}
